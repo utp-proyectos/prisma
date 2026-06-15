d661c98eb12c0ce9cd5cf29632cc8e66411d8abc
aomine <aomine1745@gmail.com>
2026-06-15 16:48:01 -0500

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,9 @@
-import { Component, signal } from '@angular/core'
+import { Component } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
-import { HlmButtonImports } from '@spartan-ng/helm/button'
-import { NgIcon, provideIcons } from '@ng-icons/core'
-import { lucideChevronRight, lucideArrowUp } from '@ng-icons/lucide'
-import { HlmIcon } from '@spartan-ng/helm/icon'
-import { HlmDialogImports } from '@spartan-ng/helm/dialog'
-import { type BrnDialogState } from '@spartan-ng/brain/dialog'
 
 @Component({
 	selector: 'app-root',
-	imports: [RouterOutlet, HlmButtonImports, HlmIcon, NgIcon, HlmDialogImports],
-	providers: [provideIcons({ lucideChevronRight, lucideArrowUp })],
+	imports: [RouterOutlet],
 	templateUrl: './app.html',
 })
-export class App {
-	protected readonly title = signal('prisma')
-
-	protected readonly modal = signal<BrnDialogState>('closed')
-}
+export class App {}
