3989036b7a5e7a9da46aeb48ce80869fb8565e4f
aomine <aomine1745@gmail.com>
2026-06-16 13:16:54 -0500

    feat: configurar rutas de auth feature

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: '',
+		pathMatch: 'full',
+		redirectTo: 'auth/login',
+	},
+	{
+		path: 'auth',
+		loadChildren: () => import('./features/auth/auth.routes').then((m) => m.authRoutes),
+	},
+]
---
+++ src/app/features/auth/auth.routes.ts
@@ -0,0 +1,19 @@
+import { Routes } from '@angular/router'
+import { Login } from './pages/login/login'
+import { Register } from './pages/register/register'
+
+export const authRoutes: Routes = [
+	{
+		path: '',
+		children: [
+			{
+				path: 'login',
+				component: Login,
+			},
+			{
+				path: 'register',
+				component: Register,
+			},
+		],
+	},
+]
