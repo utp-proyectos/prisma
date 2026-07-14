52f7e556ac498ff1d796367f49bcd4c8ca6a2204
Jhordan <calixto4211@gmail.com>
2026-07-14 09:48:32 -0500

    fixes: arreglar el estado privado de un kanban

--- src/app/features/kanban/features/kanban/kanban.realtime.ts
+++ src/app/features/kanban/features/kanban/kanban.realtime.ts
@@ -2,11 +2,13 @@ import { inject, Injectable } from '@angular/core'
 import { KanbanApi } from './kanban.api'
 import { KanbanState } from './kanban.state'
 import { Subscription } from 'rxjs'
+import { AuthService } from '@/core/servies/auth.serive'
 
 @Injectable()
 export class KanbanBoardRealtime {
 	private readonly api = inject(KanbanApi)
 	private readonly kanbanState = inject(KanbanState)
+	private readonly authService = inject(AuthService)
 
 	private kanbanSub?: Subscription
 
@@ -14,14 +16,27 @@ export class KanbanBoardRealtime {
 		this.kanbanSub?.unsubscribe()
 
 		this.kanbanSub = this.api.getKanbans(projectId, teamId).subscribe((event) => {
+			const currentUserId = this.authService.currentUser()?.id
+
 			switch (event.action) {
 				case 'CREATE':
-					console.log('CREATE', event.payload)
+					if (event.payload.privateSwitch && event.payload.creatorId !== currentUserId) {
+						break
+					}
 					this.kanbanState.addKanban(event.payload)
 					break
 
 				case 'UPDATE':
-					this.kanbanState.updateKanban(event.payload)
+					if (event.payload.privateSwitch && event.payload.creatorId !== currentUserId) {
+						this.kanbanState.removeKanban(event.payload.id)
+					} else {
+						const exists = this.kanbanState.kanbanIds().has(event.payload.id)
+						if (exists) {
+							this.kanbanState.updateKanban(event.payload)
+						} else {
+							this.kanbanState.addKanban(event.payload)
+						}
+					}
 					break
 
 				case 'DELETE':
